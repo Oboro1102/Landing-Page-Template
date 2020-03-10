2956964fed23a3072657f8ec6d5907264365bde4
Oboro Kuo <34804436+Oboro1102@users.noreply.github.com>
2020-03-10 19:52:03 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,46 +1,49 @@
-// 頁面讀取動畫
+// 起始頁面讀取動畫
 $(window).on("load", function () {
-    $('#loading').hide("fade", 300);
+    setTimeout(() => {
+        $('#loading').hide("fade", 300);
+    }, 1);
 })
 
-// 回到頂端按鈕動畫
+// 視窗滾動事件
 $(window).on("load scroll", function () {
     if ($(this).scrollTop() > 0) {
-        $('#to-top').show("fade", 300);
-        $('.main-warp__scroll').hide("fade", 300);
+        $('.toTop').show("fade");
+        $('.scrollDown').hide("fade");
+        $('.navbar').addClass('navbar--scroll');
     } else {
-        $('#to-top').hide("fade", 300);
-        $('.main-warp__scroll').show("fade", 300);
+        $('.toTop').hide("fade");
+        $('.scrollDown').show("fade");
+        $('.navbar').removeClass('navbar--scroll');
     }
 });
 
 // 回到頂端按鈕
-$("#to-top").on("click", function () {
+$(".toTop").on("click", function () {
     $("html,body").animate({
         scrollTop: 0
     }, 500);
 });
 
-// 平滑轉跳
-$(".smooth--hash").on("click", function (e) {
+// 錨點平滑轉跳
+$(".smoothHash").on("click", function (e) {
     e.preventDefault;
-    var navHeight = $(".navbar").height();
-    var offsetHeight = $($(this).attr("href")).offset().top - navHeight;
+    let navHeight = $(".navbar").outerHeight(true);
+    let offsetHeight = $($(this).attr("href")).offset().top - navHeight;
     $('html,body').animate({
         scrollTop: offsetHeight
     }, 500);
 });
 
-$(".main-warp__scroll").on("click", function (e) {
-    e.preventDefault;
-    $("html, body").animate({
-        scrollTop: $(".content-warp").offset().top
-    }, 800);
+// 產品切換顯示
+$(".product-wrap__nav__link").on("click", function () {
+    $(this).addClass("product-wrap__nav__link--active").siblings().removeClass("product-wrap__nav__link--active");
+    let itemContent = $(this).attr("data-name");
+    $(itemContent).show().siblings().hide();
 });
 
-// 產品切換顯示
-$(".detail-warp__button").on("click", function () {
-    $(this).addClass("detail-warp__button--active").siblings().removeClass("detail-warp__button--active");
-    var itemContent = $(this).attr("data-name");
-    $(itemContent).show().siblings().hide();
+// 頁腳顯示現在年度
+$('#nowTime').text(function () {
+    let date = new Date();
+    return date.getFullYear();
 });
